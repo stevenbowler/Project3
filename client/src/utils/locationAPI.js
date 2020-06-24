9af602e273d59b9188a3d3675f12078739173e23
Steven Bowler <stevenbowler@yahoo.com>
2020-06-24 12:11:55 -0500

    add client/.env.local for REACT_APP_GOOGLE_API_KEY protection, update README.md

--- client/src/utils/locationAPI.js
+++ client/src/utils/locationAPI.js
@@ -2,7 +2,7 @@
 /**@module */
 import axios from "axios";
 import { setZipCode } from '../redux/actionCreator';
-
+require('dotenv').config();
 
 /** 
 * this is object with registration data
@@ -20,14 +20,17 @@ export default {
          * @param {function} dispatch passed thru from Redux initialized Component
          * */
     findZipCode: function (dispatch) {
-        var urlFindLatLng = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDOM8_XwRnIiMs5GxUXMJy15zBz_3UwkC4";
+        // TODO Google "Geolocation API" call, key must be approved for this API
+        console.log("process.env.GOOGLE_API_KEY: ", process.env.REACT_APP_GOOGLE_API_KEY);
+        var urlFindLatLng = `https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.REACT_APP_GOOGLE_API_KEY}`;
         axios
             .post(urlFindLatLng)
             .then(response => {
                 console.log("findZipcode latlng response:", response.data.location.lat);
                 var latitude = response.data.location.lat;
                 var longitude = response.data.location.lng;
-                var urlFindZip = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyDOM8_XwRnIiMs5GxUXMJy15zBz_3UwkC4`
+                // TODO Google "Geocoding API" call, key must be approved for this API
+                var urlFindZip = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`
                 axios
                     .post(urlFindZip)
                     .then(response => {
