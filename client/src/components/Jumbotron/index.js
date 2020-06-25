cbc650816167e211e2642da61da86f73a95d4c55
robertalanis <rxalanis@gmail.com>
2020-06-24 22:09:24 -0500

    home page

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -6,7 +6,7 @@ import React from "react";
 function Jumbotron({ children }) {
 	return (
 		<div
-			style={{ height: 300, color:"forestgreen", backgroundColor:"white", clear: "both", paddingTop: 120, textAlign: "center" }}
+			style={{ height: 350, color:"forestgreen", backgroundColor:"white", clear: "both", paddingTop: 120, textAlign: "center" }}
 			className="jumbotron"
 		>
 			{children}
