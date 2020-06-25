ec9d57f1030cfc5735e6cea29a55e5d8c7b78aa0
Steven Bowler <stevenbowler@yahoo.com>
2020-06-25 10:50:47 -0500

    Revert "get rid of client/build directory"
    This reverts commit f95027f0ed9d1530f92530ba1f4d0354cf168785.

---
+++ client/build/precache-manifest.323e05be9e77c4e4d942d8ed14be5875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "abe0955afb09e4d1e075",
+    "url": "/static/css/main.8aad2134.chunk.css"
+  },
+  {
+    "revision": "abe0955afb09e4d1e075",
+    "url": "/static/js/main.560d0d61.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bbe435a471da813f56fb",
+    "url": "/static/css/2.403aaeae.chunk.css"
+  },
+  {
+    "revision": "bbe435a471da813f56fb",
+    "url": "/static/js/2.7833d9f6.chunk.js"
+  },
+  {
+    "revision": "c0866df28e5a1aa0ec9e1786d800f2f1",
+    "url": "/index.html"
+  }
+];
