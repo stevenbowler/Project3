b2866df73e0d4f3f742dedf289b0d67e691a4bd2
Steven Bowler <stevenbowler@yahoo.com>
2020-06-25 16:34:02 -0500

    update JSDOC for newzipcode module

--- client/src/utils/locationAPI.js
+++ client/src/utils/locationAPI.js
@@ -4,21 +4,20 @@ import axios from "axios";
 import { setZipCode } from '../redux/actionCreator';
 require('dotenv').config();
 
+
 /** 
 * this is object with registration data
 * @typedef {object} data
 * @property {string} [name] - 8+ digit user name regex alpha-numeric only use name on register
 * @property {string} email - email format string
 * @property {string} password - minimum 8 digit password regex alpha-numeric
-* 
 */
 
 export default {
-    /** 
-         * called from App.js componentDidMount to set currentLocationZipCode
-         * @function findZipcode
-         * @param {function} dispatch passed thru from Redux initialized Component
-         * */
+    /**Called from App.js componentDidMount to set currentLocationZipCode
+    * @function findZipcode
+    * @param {function} dispatch passed thru from Redux initialized Component
+    * */
     findZipCode: function (dispatch) {
         // TODO Google "Geolocation API" call, key must be approved for this API
         console.log("process.env.GOOGLE_API_KEY: ", process.env.REACT_APP_GOOGLE_API_KEY);
@@ -34,16 +33,14 @@ export default {
                 axios
                     .post(urlFindZip)
                     .then(response => {
-                        // console.log("findZipcode zipcode response: ", response.data.address_components[8].longform);
                         // console.log("findZipcode zipcode response: ", response.data.results[0].address_components[6].long_name);
-                        dispatch(setZipCode(response.data.results[0].address_components[6].long_name
-                        ));
+                        dispatch(setZipCode(response.data.results[0].address_components[6].long_name));
                     }).catch(error => {
-                        console.log("Could not get zipcode from locationAPI.js", error);
+                        console.log("Could not get zipcode from GeocodeAPI call in locationAPI.js", error);
                     });
             })
             .catch(error => {
-                console.log(" Could not get lat long from locationAPI.js: " + error.message);
+                console.log(" Could not get lat long from GeolocationAPI call in locationAPI.js: " + error.message);
             });
     }
 }
